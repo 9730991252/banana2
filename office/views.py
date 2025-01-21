b6e823d8d60068ebb7e451a8b5a14bdfb5a322ba
9730991252 <sunilkale101@gmail.com>
2025-01-21 18:35:00 +0530

    ok

--- office/views.py
+++ office/views.py
@@ -597,7 +597,7 @@ def new_farmer_bill(request):
             'e':e,
             'selected_farmer_status':selected_farmer_status,
             'farmer':farmer,
-            # 'leaf_weight':Farmer_services.objects.filter(shope_id=e.shope.id,name='Leaf Weight').first()
+            'leaf_weight':Farmer_services.objects.filter(shope_id=e.shope.id,name='Leaf Weight').first()
         }
         return render(request, 'office/new_farmer_bill.html', context)
     else:
